8e037c2b69d17a81f44c0252a93a8db273965458
Matt S <mjstickland@gmail.com>
2021-12-01 13:16:17 +0000

    working code using convert and read, cutoff time set to 1000 arbitrarily, using config class seems to bypass actual setup so need to manually set config variables when using convert function

--- MultiThreadTest/MultiThreadTest.ino
+++ MultiThreadTest/MultiThreadTest.ino
@@ -25,19 +25,19 @@
 #define USR_BUTTON 34
 
 /****** Global Variables ******/
-  int buttonState = 0;  // variable for reading the pushbutton status
-  bool toggle = 0;
-  bool latch = 1;
+int buttonState = 0;  // variable for reading the pushbutton status
+bool toggle = 0;
+bool latch = 1;
 
 // 0x68 is the default address for all MCP342x devices
 uint8_t address = 0x68;
 MCP342x adc = MCP342x(address);
 
 // ADC Configuration settings
-MCP342x::Config config1(MCP342x::channel2, MCP342x::oneShot,
-		       MCP342x::resolution18, MCP342x::gain1);
+MCP342x::Config config1(MCP342x::channel1, MCP342x::oneShot,
+                        MCP342x::resolution18, MCP342x::gain1);
 MCP342x::Config config2(MCP342x::channel2, MCP342x::oneShot,
-           MCP342x::resolution18, MCP342x::gain1);
+                        MCP342x::resolution18, MCP342x::gain1);
 
 // Configuration/status read back from the ADC
 MCP342x::Config status;
@@ -50,7 +50,7 @@ bool startConversion = false;
 void Humiditytest()
 {
   Serial.println("Humidity Test");/****** Debug Messege please delete when not required ******/
-  
+
   Serial.print("Humidity(%RH): ");
   Serial.print(SHT2x.GetHumidity());
   Serial.print("\nTemperature(C): ");
@@ -62,97 +62,134 @@ void Humiditytest()
 void ADCRead()
 {
   //Serial.println("ADC Test Channel 1");/****** Debug Messege please delete when not required ******/
-  
+
   long value = 0;
   uint8_t err;
 
-  if (startConversion) {
-    //Serial.println("Convert");
-    //adc.configure(config);
-    err = adc.convert(config1);
-    if (err) {
-      Serial.print("Convert error: ");
-      Serial.println(err);
-    }
-    startConversion = false;
-  }
+  //  if (startConversion) {
+  //    //Serial.println("Convert");
+  //    err = adc.convert(adc.channel2, adc.oneShot,
+  //           adc.resolution18, adc.gain1);
+  //    if (err) {
+  //      Serial.print("Convert error: ");
+  //      Serial.println(err);
+  //    }
+  //    startConversion = false;
+  //  }
+  //
+  //  delay(10); // wait for conversion to occur
+  //
+  //  err = adc.read(value, status);
+  //  if (!err && status.isReady()) {
+  //    // For debugging purposes print the return value.
+  //    Serial.print("Light: ");
+  //    Serial.println(value);
+  //    Serial.print("Config: 0x");
+  //    Serial.println((int)config1, HEX);
+  //    Serial.print("Convert error: ");
+  //    Serial.println(err);
+  //    startConversion = true;
+  //  }
+  //  else
+  //  {
+  //    Serial.println("data not ready1");
+  //  }
+  //*************************** Convert and read Tests ****************************/
 
-  delay(5); // wait for conversion to occur
-  
-  err = adc.read(value, status);
-  if (!err && status.isReady()) { 
+  err = adc.convertAndRead(adc.channel1, adc.oneShot, adc.resolution18, adc.gain2, 1000, value, status);
+  if (!err && status.isReady()) {
     // For debugging purposes print the return value.
     Serial.print("Light: ");
     Serial.println(value);
-    //Serial.print("Config: 0x");
-    //Serial.println((int)config1, HEX);
-    //Serial.print("Convert error: ");
-    //Serial.println(err);
+    Serial.print("Config: 0x");
+    Serial.println((int)config1, HEX);
+    Serial.print("Convert error: ");
+    Serial.println(err);
     startConversion = true;
   }
   else
   {
-    Serial.println("data not ready");
-  }
-/************************** Read channel 2 ***********************/
-   // Serial.println("ADC Test Channel 2");/****** Debug Messege please delete when not required ******/
-  
-  value = 0;
-  err = 0;
-
-  if (startConversion) {
-    //Serial.println("Convert");
-    err = adc.convert(config2);
-    if (err) {
-      Serial.print("Convert error: ");
-      Serial.println(err);
-    }
-    startConversion = false;
+    Serial.println("data not ready1");
   }
 
-  delay(5); // wait for conversion to occur
-  
-  err = adc.read(value, status);
-  if (!err && status.isReady()) { 
+  delay(100);
+
+  err = adc.convertAndRead(adc.channel2, adc.oneShot, adc.resolution18, adc.gain1, 1000, value, status);
+  if (!err && status.isReady()) {
     // For debugging purposes print the return value.
     Serial.print("Moisture: ");
     Serial.println(value);
-    //Serial.print("Config: 0x");
-    //Serial.println((int)config2, HEX);
-    //Serial.print("Convert error: ");
-   // Serial.println(err);
+    Serial.print("Config: 0x");
+    Serial.println((int)config1, HEX);
+    Serial.print("Convert error: ");
+    Serial.println(err);
     startConversion = true;
   }
   else
   {
-    Serial.println("data not ready");
+    Serial.println("data not ready1");
   }
+
+  ///************************** Read channel 2 ***********************/
+  //   // Serial.println("ADC Test Channel 2");/****** Debug Messege please delete when not required ******/
+  //
+  //  value = 0;
+  //  err = 0;
+  //
+  //  if (startConversion) {
+  //    //Serial.println("Convert");
+  //    err = adc.convert(adc.channel2, adc.oneShot,
+  //           adc.resolution18, adc.gain1);
+  //    if (err) {
+  //      Serial.print("Convert error: ");
+  //      Serial.println(err);
+  //    }
+  //    startConversion = false;
+  //  }
+  //
+  //  delay(10); // wait for conversion to occur
+  //
+  //  err = adc.read(value, status);
+  //  if (!err && status.isReady()) {
+  //    // For debugging purposes print the return value.
+  //    Serial.print("Moisture: ");
+  //    Serial.println(value);
+  //    Serial.print("Config: 0x");
+  //    Serial.println((int)config2, HEX);
+  //    Serial.print("Convert error: ");
+  //    Serial.println(err);
+  //    startConversion = true;
+  //  }
+  //  else
+  //  {
+  //    Serial.println("data not ready2");
+  //  }
 }
 
 /****** Function: Read Button Value include debounce ******/
 
 bool Read_Button()
 {
+  buttonState = digitalRead(USR_BUTTON);
+
+  if (buttonState == HIGH)
+  {
+    delay(5);
     buttonState = digitalRead(USR_BUTTON);
-      
     if (buttonState == HIGH)
     {
-      delay(5);
-      buttonState = digitalRead(USR_BUTTON);
-      if(buttonState == HIGH)
-      {
-        //Serial.println("Button HIGH");/****** Debug Messege please delete when not required ******/
-        return HIGH;
-      }
+      //Serial.println("Button HIGH");/****** Debug Messege please delete when not required ******/
+      return HIGH;
     }
-    //Serial.println("Button LOW");/****** Debug Messege please delete when not required ******/
-    return LOW;
+  }
+  //Serial.println("Button LOW");/****** Debug Messege please delete when not required ******/
+  return LOW;
 }
 
 /****** Threading Setup ******/
 
-TimedAction ADCThread = TimedAction(500,ADCRead);
-TimedAction humidityThread = TimedAction(500,Humiditytest);
+TimedAction ADCThread = TimedAction(1000, ADCRead);
+TimedAction humidityThread = TimedAction(1000, Humiditytest);
 
 /****** Arduino Setup Function ******/
 
@@ -160,16 +197,16 @@ void setup(void)
 {
   Serial.begin(9600);
   Wire.begin();
-  
+
   Serial.println("Begun Serial communications");/****** Debug Messege please delete when not required ******/
-    
+
   // Reset devices
   MCP342x::generalCallReset();
   delay(1); // MC342x needs 300us to settle
-  
+
   // Check device present
   Wire.requestFrom(address, (uint8_t)1);
-  if (!Wire.available()) 
+  if (!Wire.available())
   {
     Serial.print("No device found at address ");
     Serial.println(address, HEX);
@@ -179,25 +216,25 @@ void setup(void)
     }
   }
 
-   adc.configure(config2);
+  adc.configure(config2);
 
   pinMode(ENABLE_MOT, OUTPUT);
   pinMode(SLEEP_MOT, OUTPUT);
   digitalWrite(ENABLE_MOT, LOW);
   digitalWrite(SLEEP_MOT, HIGH);
-  
+
   pinMode(dir_mot1, OUTPUT);
   pinMode(step_mot1, OUTPUT);
   digitalWrite(dir_mot1, HIGH);
-  
+
   pinMode(dir_mot2, OUTPUT);
   pinMode(step_mot2, OUTPUT);
   digitalWrite(dir_mot2, HIGH);
-  
+
   pinMode(dir_mot3, OUTPUT);
   pinMode(step_mot3, OUTPUT);
   digitalWrite(dir_mot3, HIGH);
-  
+
   pinMode(dir_mot4, OUTPUT);
   pinMode(step_mot4, OUTPUT);
   digitalWrite(dir_mot4, HIGH);
@@ -212,18 +249,19 @@ void setup(void)
   startConversion = true;
 }
 
-/****** Main Loop ******/ 
+/****** Main Loop ******/
 
 void loop(void) {
 
   ADCThread.check();
+  delay(400);
   humidityThread.check();
-  
-    if(Read_Button())
-    {
-        digitalWrite(step_mot1, HIGH);   
-        delay(1);                     
-        digitalWrite(step_mot1, LOW);    
-        delay(1);     
-    }   
+
+  if (Read_Button())
+  {
+    digitalWrite(step_mot1, HIGH);
+    delay(1);
+    digitalWrite(step_mot1, LOW);
+    delay(1);
+  }
 }
